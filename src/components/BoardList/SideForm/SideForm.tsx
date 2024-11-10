dcde326f9e2f6e52da3bc03347f5c626c667824e
Algoruu <zlfkcmspdbdp@naver.com>
2024-11-11 03:37:06 +0900

    SideForm Style 생성하기

--- src/components/BoardList/SideForm/SideForm.tsx
+++ src/components/BoardList/SideForm/SideForm.tsx
@@ -1,5 +1,10 @@
 import React, { ChangeEvent, FC, useRef, useState } from 'react'
 import { FiCheck } from 'react-icons/fi';
+import { icon, input, sideForm } from './SideForm.css';
+import { useTypedDispatch } from '../../../hooks/redux';
+import { addBoard } from '../../../store/slices/boardsSlice';
+import { v4 as uuidv4 } from 'uuid';
+import { addLog } from '../../../store/slices/loggerSlice';
 
 type TSideFormOpen = {
   inputRef : React.RefObject<HTMLInputElement>,
@@ -11,6 +16,7 @@ const SideForm: FC<TSideFormOpen> = ({
   inputRef
 }) => {
   const [inputText, setInputText] = useState('');
+  const dispatch = useTypedDispatch();
   
   const handleChange = (e : ChangeEvent<HTMLInputElement>) => {
     setInputText(e.target.value);
@@ -20,18 +26,42 @@ const SideForm: FC<TSideFormOpen> = ({
     setIsFormOpen(false);
   }
 
+  const handleClick = () => {
+    if(inputText) {
+      dispatch(
+        addBoard({
+          board: {
+            boardId : uuidv4(),
+            boardName : inputText,
+            lists : []
+          }
+        })
+      )
+
+        dispatch(
+          addLog({
+            logId : uuidv4,
+            logMessage : `게시판 등록 : ${inputText}`,
+            logAuthor : "User",
+            logTimestamp : String(Date.now()),
+          })
+        )
+    }
+  }
+
   return (
-    <div>
+    <div className={sideForm}>
       <input
         // ref={inputRef}
         autoFocus
+        className={input}
         type='text'
         placeholder='새로운 게시판 등록하기'
         value={inputText}
         onChange={handleChange}
         onBlur={handleOnBlur}
       />
-      <FiCheck />
+      <FiCheck className={icon} onMouseDown={handleClick}/>
     </div>
   )
 }
